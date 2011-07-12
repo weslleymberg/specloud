10e503db6180cb3c6658919dff72f3d624b4d4b6
bernardofire <bernardo-fire@hotmail.com>
2011-07-13 04:33:27 +0800

    run nose direct instead of call a subprocess

--- specloud/__init__.py
+++ specloud/__init__.py
@@ -1,15 +1,13 @@
 import sys
-import subprocess
+import nose
 
-def run():
-    return subprocess.call(['nosetests',
-            '-i',
-            '^(it|ensure|must|should|deve|specs?|examples?)',
-            '-i',
-            '(specs?|examples?|exemplos?)(.py)?$',
+def run(argv=[]):
+    return nose.run(argv=[
+            '-i ^(it|ensure|must|should|deve|specs?|examples?)',
+            '-i (specs?|examples?|exemplos?)(.py)?$',
             '--with-spec',
             '--spec-color',
-            ] + sys.argv[1:])
+            ] + argv)
 
 def main():
-    sys.exit(run())
+    sys.exit(run(sys.argv[1:]))
