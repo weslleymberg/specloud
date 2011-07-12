bf75d960a688e6dc0bd29f744a77f2e355f214c0
bernardofire <bernardo-fire@hotmail.com>
2011-07-12 09:01:00 +0800

    created run function to run specloud without exit python

--- specloud/__init__.py
+++ specloud/__init__.py
@@ -1,15 +1,15 @@
 import sys
 import subprocess
 
+def run():
+    return subprocess.call(['nosetests',
+            '-i',
+            '^(it|ensure|must|should|deve|specs?|examples?)',
+            '-i',
+            '(specs?|examples?|exemplos?)(.py)?$',
+            '--with-spec',
+            '--spec-color',
+            ] + sys.argv[1:])
 
 def main():
-    sys.exit(
-            subprocess.call(['nosetests',
-                    '-i',
-                    '^(it|ensure|must|should|deve|specs?|examples?)',
-                    '-i',
-                    '(specs?|examples?|exemplos?)(.py)?$',
-                    '--with-spec',
-                    '--spec-color',
-                    ] + sys.argv[1:]))
-
+    sys.exit(run())
